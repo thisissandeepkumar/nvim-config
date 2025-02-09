38d72c2dbae4435b449594e69509947cc3fffdf2
Sandeep Kumar <sandeepmahendran@gmail.com>
2025-02-09 19:17:44 +0530

    Add settings for running cpp

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.21.2
 
-RUN apk add --no-cache llvm clang gdb
+RUN apk add --no-cache llvm clang gdb clang-extra-tools build-base gcc musl-dev
 
 RUN clang --version
 
@@ -9,14 +9,5 @@ WORKDIR /app
 RUN apk add --no-cache build-base neovim git bash curl ncurses tree
 
 RUN git clone --depth 1 https://github.com/wbthomason/packer.nvim /root/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-RUN set -ex && \
-    apk add --no-cache gcc musl-dev
-
-RUN set -ex && \
-    rm -f /usr/libexec/gcc/x86_64-alpine-linux-musl/6.4.0/cc1obj && \
-    rm -f /usr/libexec/gcc/x86_64-alpine-linux-musl/6.4.0/lto1 && \
-    rm -f /usr/libexec/gcc/x86_64-alpine-linux-musl/6.4.0/lto-wrapper && \
-    rm -f /usr/bin/x86_64-alpine-linux-musl-gcj
 
 CMD ["sh", "-c", "while :; do sleep 1; done"]
--- init.lua
+++ init.lua
@@ -6,13 +6,15 @@ require('packer').startup(function(use)
     use 'hrsh7th/nvim-cmp'              -- Autocompletion
     use 'hrsh7th/cmp-nvim-lsp'          -- LSP source for nvim-cmp
     use 'L3MON4D3/LuaSnip'              -- Snippets
+    use 'saadparwaiz1/cmp_luasnip'      -- Snippet source for nvim-cmp
     use 'nvim-telescope/telescope.nvim' -- Fuzzy finder
     use 'nvim-lualine/lualine.nvim'     -- Statusline
     use 'kyazdani42/nvim-tree.lua'      -- File Explorer
     use 'lewis6991/gitsigns.nvim'       -- Git integration
     use 'gruvbox-community/gruvbox'    -- Gruvbox colorscheme
-    use 'mfussenegger/nvim-dap'
-    use 'rcarriga/nvim-dap-ui'
+    use 'mfussenegger/nvim-dap'         -- Debug Adapter Protocol
+    use 'rcarriga/nvim-dap-ui'          -- Debug UI for DAP
+    use 'nvim-neotest/nvim-nio'         -- Required for nvim-dap-ui
 end)
 
 -- Basic Neovim settings
@@ -64,10 +66,30 @@ for _, server in ipairs(servers) do
 end
 
 -- Statusline setup
-require('lualine').setup()
+require('lualine').setup {
+    options = {
+        theme = 'gruvbox',
+        section_separators = '',
+        component_separators = '|',
+    },
+}
 
 -- File Explorer setup
-require('nvim-tree').setup()
+require('nvim-tree').setup {
+    view = {
+        width = 30,
+        side = 'left',
+    },
+}
+
+-- Gitsigns Configuration
+require('gitsigns').setup {
+    signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+    },
+}
 
 -- Colorscheme setup with safe fallback
 local colorscheme = "gruvbox"
@@ -76,3 +98,75 @@ if not ok then
     vim.notify("Colorscheme " .. colorscheme .. " not found! Defaulting to 'default'.", vim.log.levels.WARN)
     vim.cmd("colorscheme default") -- Fallback to default
 end
+
+-- Setup nvim-cmp
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `LuaSnip` users.
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item.
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' }, -- LSP-based completions
+    { name = 'luasnip' },  -- Snippet completions
+  }, {
+    { name = 'buffer' },   -- Buffer-based completions
+    { name = 'path' },     -- File path completions
+  })
+})
+
+-- Use buffer source for `/` and `?`
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for `:`
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Debugging (nvim-dap) Configuration
+local dap = require('dap')
+local dapui = require('dapui')
+
+dapui.setup()
+dap.listeners.after.event_initialized['dapui_config'] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated['dapui_config'] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited['dapui_config'] = function()
+  dapui.close()
+end
+
+-- LSP Setup
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig').clangd.setup {
+  capabilities = capabilities,
+}
+
+-- Keybinding to compile and run main.cpp
+vim.api.nvim_set_keymap(
+  'n', 
+  '<leader>r', 
+  ':w<CR>:!g++ -std=c++17 % -o %:r && ./%:r<CR>', 
+  { noremap = true, silent = true }
+)
